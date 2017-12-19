0e9acb356e7782e391ed154e10cd5aa68f73ea8a
Alex Makovsky <alex.m.sky@gmail.com>
2017-12-19 22:25:04 +0300

    шлифовка > модалки и посадка

--- app/resources/js/scripts.js
+++ app/resources/js/scripts.js
@@ -1,13 +1,24 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
-// 	var titleWidth = document.getElementsByClassName('advtgs__item-text');
-// 	for(i=0;i<titleWidth.length;i++) {
-// 		var titleWidthWidth = titleWidth[i];
-// 		console.log(titleWidth);
-// 	}
-// });
-//
-//
-// document.addEventListener("DOMContentLoaded", function(event) {
-// 	var tWidth = document.getElementById('advtgs').offsetWidth;
-// 	console.log(tWidth);
-// });
+// Прокрутка к началу страницы
+$(document).ready(function(){
+
+	if ($(this).scrollTop() > 100) {
+			$('.footer__to-top').fadeIn();
+	} else {
+			$('.footer__to-top').fadeOut();
+	}
+
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+          $('.footer__to-top').fadeIn();
+      } else {
+          $('.footer__to-top').fadeOut();
+      }
+  });
+
+  //Click event to scroll to top
+  $('.footer__to-top').click(function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+  });
+});
